e9cd9ad30a7eae170edbc149087781982b4299b7
Luis <luiisflorido@gmail.com>
2020-10-22 02:40:05 -0300

    feat: adiciona campo active aos htmls relacionados ao owner, adiciona campo activeTypes que serve como variável para o input na tela de cadastro/edição, também remove NotEmpty do model owner devido a erro

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -51,7 +51,6 @@ public class Owner extends Person {
 	private String telephone;
 
 	@Column(name = "active")
-	@NotEmpty
 	private Boolean active;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
